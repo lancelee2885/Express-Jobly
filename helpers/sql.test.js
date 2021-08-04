eee6a7cce0b5b9e3f629933e0e2408bfa4c999fa
lancelee2885 <lancelee2885@gmail.com>
2021-08-04 11:01:09 -0700

    test for sql.js

---
+++ helpers/sql.test.js
@@ -0,0 +1,26 @@
+const { sqlForPartialUpdate } = require("./sql")
+
+describe("sqlForPartialUpdate", function (){
+  test("valid update", function(){
+    const data = {firstName: 'Aliya', age: 32};
+    const sqlCols = {
+      firstName: "first_name"
+    };
+
+    const result = sqlForPartialUpdate(data,sqlCols);
+    expect(result).toEqual({
+      setCols: '"first_name"=$1, "age"=$2',
+      values: ["Aliya", 32]
+    })
+  })
+  test("invalid update", function(){
+    const data = {};
+    const sqlCols = {};
+
+    try {
+      const result = sqlForPartialUpdate(data,sqlCols);
+    } catch(e){
+      expect(e.message).toEqual("No data")
+    }
+  })
+})
