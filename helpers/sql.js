4e302354dda1d39fbdc0842398b6394035ac5696
lancelee2885 <lancelee2885@gmail.com>
2021-08-04 11:00:51 -0700

    docstrings added

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,19 @@
 const { BadRequestError } = require("../expressError");
 
 // THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Given an object of data
+ *  {firstName: 'Aliya', age: 32},
+ *  Return an object with keys of psql SET clause
+ *  and an array of values (['"first_name"=$1', '"age"=$2'])
+ * 
+ * @param {object}dataToUpdate: an obj containing keys columns to update and
+ * values of what's going to be updated
+ * {firstName: 'Aliya', age: 32}
+ * 
+ * @param {object}jsToSql converting camelCase javascript variable to snakeCase for psql query
+ * numEmployees > num_employees
+ * logoUrl > logo_url
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
@@ -12,8 +25,8 @@ function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   );
 
   return {
-    setCols: cols.join(", "),
-    values: Object.values(dataToUpdate),
+    setCols: cols.join(", "), // SET clause of updating database
+    values: Object.values(dataToUpdate), // parametized array
   };
 }
 
